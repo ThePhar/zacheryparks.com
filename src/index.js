571a83f6f02f2dffc80a12732dea374dbb1d24b1
Zach Parks <zach@alliware.com>
2019-07-24 20:00:35 -0500

    modified easter egg to seperate script

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './stylesheets/index.sass';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+import EasterEgg from "./easteregg";
+
 ReactDOM.render(<App />, document.getElementById('root'));
+EasterEgg(); // Shhh... Don't worry about it.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
